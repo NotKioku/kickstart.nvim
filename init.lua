99846642f8eafeb54b39491e1776cc226a763587
NotKioku <cjy01761540@gmail.com>
2025-06-12 08:51:22 +0800

    gruvbox

--- init.lua
+++ init.lua
@@ -850,10 +850,36 @@ require('lazy').setup({
   },
 
   {
-    'morhetz/gruvbox',
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
     config = function()
-      -- vim.cmd.colorscheme 'gruvbox'
+      require('gruvbox').setup {
+        terminal_colors = true, -- add neovim terminal colors
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = true,
+          emphasis = true,
+          comments = true,
+          operators = false,
+          folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = '', -- can be "hard", "soft" or empty string
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false,
+      }
+      vim.o.background = 'dark'
+      vim.cmd.colorscheme 'gruvbox'
     end,
+    opts = ...,
   },
 
   {
@@ -864,7 +890,7 @@ require('lazy').setup({
       require('bamboo').setup {
         -- optional configuration here
       }
-      require('bamboo').load()
+      -- vim.cmd.colorscheme 'bamboo'
     end,
   },
 
