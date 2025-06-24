8f23032a9e45367e8e10df65a67a4079a5d39e81
NotKioku <cjy01761540@gmail.com>
2025-06-24 18:44:18 +0800

    colorscheme?

--- init.lua
+++ init.lua
@@ -883,6 +883,19 @@ require('lazy').setup({
   },
 
   {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_background = 'soft'
+      vim.g.gruvbox_material_enable_italic = true
+      -- vim.cmd.colorscheme 'gruvbox-material'
+    end,
+  },
+
+  {
     'ribru17/bamboo.nvim',
     lazy = false,
     priority = 1000,
